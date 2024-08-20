d3e19b8782997020a012a75c8e07c5d94d21002f
mirzauu <alimirsa123@gmail.com>
2024-08-21 01:01:09 +0530

    model rechange

--- Medic/user/admin.py
+++ Medic/user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile,TestDetails
+from .models import UserProfile,TestDetails,Doctor,Tests
 
 
 admin.site.register(UserProfile)  
 admin.site.register(TestDetails)  
+admin.site.register(Doctor)  
+admin.site.register(Tests)  
--- Medic/user/models.py
+++ Medic/user/models.py
@@ -10,12 +10,30 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
     
+    
+class Tests(models.Model):
+    Test_Description = models.CharField(max_length=255)
+    Biological_Reference_Interval = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.Test_Description
+    
 
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name        
+    
 class TestDetails(models.Model):
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='test_details')
+    tests = models.ManyToManyField(Tests, related_name='test_details')
     sample_collected_at = models.DateTimeField()
     age = models.PositiveIntegerField()
-    doctor = models.CharField(max_length=255)
-
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='test_details')
+    Value_Observed = models.IntegerField()
+    
     def __str__(self):
-        return f"Test for {self.user_profile.user.username} on {self.sample_collected_at}"    
+        return f"Test for {self.user_profile.user.username} on {self.sample_collected_at}"
+
